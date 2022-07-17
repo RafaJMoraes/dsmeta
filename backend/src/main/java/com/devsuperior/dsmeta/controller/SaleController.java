a88d617d92474a1e176de379f929a65abfc73433
Rafael Moraes <rmoraes@persys.com.br>
2022-07-16 22:36:46 -0300

    date select

--- backend/src/main/java/com/devsuperior/dsmeta/controller/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controller/SaleController.java
@@ -3,8 +3,11 @@ package com.devsuperior.dsmeta.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsmeta.domain.Sale;
@@ -19,7 +22,10 @@ public class SaleController {
 	private SaleService service;
 	
 	@GetMapping
-	public List<Sale> getSales(){
-		return service.findSales();
+	public Page<Sale> getSales(
+			@RequestParam(value = "minDate",defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate,
+			Pageable pageable){
+		return service.findSales(minDate, maxDate, pageable);
 	}
 }
--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,8 +1,14 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.Clock;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.domain.Sale;
@@ -17,9 +23,15 @@ public class SaleService {
 	
 	
 	
-	public List<Sale> findSales() {
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
 		
-		List<Sale> sales = saleRepository.findAll();
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		LocalDate lastYear = LocalDate.now();
+		
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		
+		Page<Sale> sales = saleRepository.findSales(min, max, pageable);
 		
 		return sales;
 		
